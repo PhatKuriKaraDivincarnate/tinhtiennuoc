4391d8740685156376b9cbeb46a5b4c28427102a
phat0988023975 <K215480106116@tnut.edu.vn>
2024-09-08 20:39:21 +0700

    Add project files.

---
+++ tinhtiennuoc/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+// Họ và tên : Nguyễn CHí Phát
+// Bắt đầu làm bài toán : tính tiền nước 
+
+namespace tinhtiennuoc
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
