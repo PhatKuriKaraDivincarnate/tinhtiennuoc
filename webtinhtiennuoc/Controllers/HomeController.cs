6de15ad9e36d64199dc151e27ce3a86c891b2a87
phat0988023975 <K215480106116@tnut.edu.vn>
2024-09-09 21:26:03 +0700

    Web tính tiền nước

---
+++ webtinhtiennuoc/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using webtinhtiennuoc.Models;
+using lib_tinhtiennuoc;
+
+namespace webtinhtiennuoc.Controllers
+{
+    public class HomeController : Controller
+    {
+        // Action hiển thị form nhập liệu
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // Action xử lý logic tính tiền nước khi người dùng nhấn nút "Tính tiền"
+        [HttpPost]
+        public IActionResult Calculate(double soM3)
+        {
+            // Kiểm tra số m3 nước nhập vào phải lớn hơn hoặc bằng 0
+            if (soM3 < 0)
+            {
+                ViewBag.Error = "Số m3 nước không được nhỏ hơn 0. Vui lòng nhập lại!";
+                return View("Index");
+            }
+
+            // Sử dụng thư viện tính tiền nước
+            Class1 tinhTien = new Class1();
+
+            // Tính tiền nước
+            double tongTien = tinhTien.TinhTien(soM3);
+
+            // Trả kết quả về view
+            ViewBag.SoM3 = soM3;
+            return View("Index", tongTien);
+        }
+    }
+}
